f6b4272a674d58a2f4fdb525cfc8d078eade7ba3
Midori Minato <midori.minato.99@gmail.com>
2022-05-22 13:51:29 +0900

    Build 1.10.0+OpenSSL_1_1_1o

--- Package.swift
+++ Package.swift
@@ -3,13 +3,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "CSSH",
+    name: "SSH2",
     products: [
-        .library(name: "CSSH", targets: ["CSSH"])
+        .library(name: "libssh2", targets: ["libssh2"]),
+        .library(name: "libcrypto", targets: ["libcrypto"]),
+        .library(name: "libssl", targets: ["libssl"])
     ],
     targets: [
-        .binaryTarget(name: "CSSH",
-                      url: "https://github.com/DimaRU/Libssh2Prebuild/releases/download/1.10.0+OpenSSL_1_1_1l/CSSH-1.10.0+OpenSSL_1_1_1l.xcframework.zip",
-                      checksum: "a4aff717265efc34f494618a489fdcaec7f50cf770343449e72113cf794c0ca5")
+        .binaryTarget(name: "libssh2",
+                      url: "https://github.com/MidoriMinato/Libssh2Prebuild/releases/download/1.10.0+OpenSSL_1_1_1o/libssh2-1.10.0+OpenSSL_1_1_1o.xcframework.zip",
+                      checksum: "58531617282b8a030debf834d65b641587904c5f7ec3976dcf9b5c9540be9497"),
+        .binaryTarget(name: "libcrypto",
+                      url: "https://github.com/MidoriMinato/Libssh2Prebuild/releases/download/1.10.0+OpenSSL_1_1_1o/libcrypto-1.10.0+OpenSSL_1_1_1o.xcframework.zip",
+                      checksum: "7fc3d58b8a0cbdbd3e3b9b798690da74ded6edaa5ed14a2eed42d7197ffa61e3"),
+        .binaryTarget(name: "libssl",
+                      url: "https://github.com/MidoriMinato/Libssh2Prebuild/releases/download/1.10.0+OpenSSL_1_1_1o/libssl-1.10.0+OpenSSL_1_1_1o.xcframework.zip",
+                      checksum: "24cec88e45e7ca4912caaadfc9f9708c8f66d2faa39f67eebe56c58972cffcb2")
     ]
 )
